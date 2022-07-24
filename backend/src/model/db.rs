0cc7f6d26d23e2786ed675a383dbef3b29670f5a
vyekahs <vyekahs@naver.com>
2022-07-24 23:39:11 +0900

    version change

--- backend/src/model/db.rs
+++ backend/src/model/db.rs
@@ -19,11 +19,11 @@ pub async fn init_db() -> Result<Db, sqlx::Error> {
 
 async fn new_db_pool(host: &str, db: &str, user: &str, pwd: &str, max_con: u32) -> Result<Db, sqlx::Error> {
     let con_string = format!("postgres://{}:{}@{}/{}", user, pwd, host, db);
-	PgPoolOptions::new()
-		.max_connections(max_con)
-		.acquire_timeout(Duration::from_millis(500)) 
-		.connect(&con_string)
-		.await
+    PgPoolOptions::new()
+    .max_connections(max_con)
+    .connect_timeout(Duration::from_millis(500))
+    .connect(&con_string)
+    .await
 }
 
 
