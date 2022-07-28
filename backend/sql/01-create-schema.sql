ef4b8f2289f9f97b6df9f90f0c57edd59fc770b5
vyekahss <vyekahs@naver.com>
2022-07-28 23:36:11 +0900

    error fix

--- backend/sql/01-create-schema.sql
+++ backend/sql/01-create-schema.sql
@@ -1,13 +1,14 @@
-CREATE TYPE todo_status_enum AS ENUM {
+CREATE TYPE todo_status_enum AS ENUM (
     'open',
     'close'
-}
+);
 
-CREATE TABLE todo {
+CREATE TABLE todo (
     id bigserial,
     cid bigint NOT NULL,
-    ctime timestamp with time zone DEFAUL now(),
+    ctime timestamp with time zone DEFAULT now(),
+    title text NOT NULL,
     status todo_status_enum NOT NULL DEFAULT 'open'
-};
+);
 
 ALTER SEQUENCE todo_id_seq RESTART WITH 1000;
